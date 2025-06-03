6d8329cfb44f5a175be56fcb980af1ef706d564c
unknown <anudeepgunda@gmail.com>
2025-06-02 21:31:17 -0500

    python basics

---
+++ week1_basics/hello.py
@@ -0,0 +1,15 @@
+print("Hello, python World!")
+
+age=30
+name="Alice"
+salary=120
+tax= 200
+
+print(f"Name: {name} Age: {age} Salary: {salary} Tax: {tax}")
+print(f"Type of Name:{type(age)}")
+print(f"Type of Age {type(name)}")
+print(f"Total Salary:{salary + tax}")
+
+greeting = "Hi, "
+full_greetings = greeting + name
+print(full_greetings)
